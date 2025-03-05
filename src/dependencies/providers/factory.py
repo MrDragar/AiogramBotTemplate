a6724c90f6b661da3812eff741564b8a826a51a5
mrdragar <yshhenyaev@mail.ru>
2025-03-06 01:40:08 +0300

    Add factory provider

---
+++ src/dependencies/providers/factory.py
@@ -0,0 +1,17 @@
+from typing import Callable, Any
+
+from provider import Provider, T
+
+
+class Factory(Provider[T]):
+    __args: tuple[Any, ...]
+    __kwargs:  dict[Any, ...]
+
+    def __init__(self, provides: Callable[..., T] | T, *args, **kwargs):
+        super().__init__(provides)
+        self.__args = args
+        self.__kwargs = kwargs
+
+    def __call__(self) -> Callable[..., T] | T:
+        return self._provides(*self.__args, **self.__kwargs)
+
