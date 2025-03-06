ee7121bcbaeded81077725d64baf0b728618fe66
mrdragar <yshhenyaev@mail.ru>
2025-03-06 17:05:24 +0300

    Add object provider

---
+++ src/dependencies/providers/object.py
@@ -0,0 +1,9 @@
+from provider import Provider, T
+
+
+class Object(Provider[T]):
+    def __init__(self, provides: T):
+        super().__init__(provides)
+
+    def __call__(self) -> T:
+        return self._provides
