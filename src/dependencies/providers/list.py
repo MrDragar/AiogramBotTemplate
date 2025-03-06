8ef5d1a29a2b6399f8bc0ecfb918093c4d775a3f
mrdragar <yshhenyaev@mail.ru>
2025-03-06 17:07:34 +0300

    Add list provider

---
+++ src/dependencies/providers/list.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+from provider import Provider
+
+
+class List(Provider[list[Any]]):
+    def __init__(self, *provides: tuple[Any]):
+        super().__init__(provides)
+
+    def __call__(self) -> list[Any]:
+        return self._provide_args(*self._provides)
